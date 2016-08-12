bcc8584d76a1aa90b8adc3b5c7bee6948e0a778e
Jeff Jones <spjeff@spjeff.com>
2016-08-12 14:33:52 -0500

    Code
    * export Office 365 user profiles to CSV
    * Azure AD
    * SharePoint UPS - User Profile Service

---
+++ office365-export-profiles/office365-export-profiles.ps1
@@ -0,0 +1,51 @@
+# Export Office 365 user profiles to CSV (Azure AD & SharePoint UPS)
+
+# Tenant
+$tenantUrl = "https://tenant-admin.sharepoint.com/"
+$userName = "admin@tenant.onmicrosoft.com" 
+$password = "pass@word1"
+
+# Azure AD (AAD) - All Profiles
+Write-Host "Azure AD (AAD)"
+$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($userName, $secPassword)
+Connect-MsolService -Credential $cred
+$allUsers = Get-MsolUser -All
+$allUsers | Export-Csv "azure-ad-profiles.csv"
+
+# SharePoint Online (SPO) - All Profiles
+Write-Host "SharePoint Online (SPO)"
+$start = Get-Date
+$i = 0
+$total = $allUsers.Count
+$coll = @()
+$csv = ""
+Connect-SPOnline -Url $tenantUrl -Credentials $cred
+foreach ($u in $allUsers) {
+	# Progress Tracking
+	$i++
+	$prct = [Math]::Round((($i / $total) * 100.0), 2)
+	$elapsed = (Get-Date) - $start
+	$remain = ($elapsed.TotalSeconds) / ($prct / 100.0)
+	$eta = (Get-Date).AddSeconds($remain)
+	
+	# Display
+	$file = $site.Url.Split('/')[4]
+	Write-Progress -Activity "Download SharePoint Online user profiles - ETA $eta" -Status "$prct" -PercentComplete $prct
+	
+	# Append CSV sign in name
+	$csv += $u.SignInName + ","
+	if ($i -eq 200) {
+		# Download
+		$csv = $csv.TrimEnd(",")
+		$obj = Get-SPOUserProfileProperty -Account $csv.Split(",") -ErrorAction SilentlyContinue
+		$coll += $obj
+		
+		# clear
+		$csv = ""
+		$i = 0
+		Write-Host "." -NoNewLine
+	}
+	$i++
+}
+$coll | Export-Csv "sharepoint-profiles.csv"

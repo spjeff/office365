e45983e3ad91206d6094cb2e17681c70094cbf5b
Jeff Jones <spjeff@spjeff.com>
2016-11-08 14:53:13 -0600

    Code
    * provision personal OneDrive sites

---
+++ office365-export-profiles/create-personal-site-enqueue-bulk.ps1
@@ -0,0 +1,29 @@
+﻿# from https://blogs.msdn.microsoft.com/frank_marasco/2014/03/25/so-you-want-to-programmatically-provision-personal-sites-one-drive-for-business-in-office-365/
+
+# tenant
+$webUrl = "https://tenant-admin.SharePoint.com"
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($webUrl)
+
+# admin user
+$web = $ctx.Web
+$username = "admin@tenant.onmicrosoft.com"
+$password = read-host -AsSecureString
+
+# context
+$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username,$password )
+$ctx.Load($web)
+$ctx.ExecuteQuery()
+
+# assembly
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.UserProfiles")
+$loader =[Microsoft.SharePoint.Client.UserProfiles.ProfileLoader]::GetProfileLoader($ctx)
+
+#To Get Profile
+$profile = $loader.GetUserProfile()
+$ctx.Load($profile)
+$ctx.ExecuteQuery()
+$profile 
+
+#To enqueue Profile
+$loader.CreatePersonalSiteEnqueueBulk(@("admin@tenant.onmicrosoft.com")) 
+$loader.Context.ExecuteQuery()

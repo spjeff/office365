25ac62baa7362d9edfc0e0fa0f93e8a31a668ffb
Jeff Jones <spjeff@spjeff.com>
2016-12-02 11:10:07 -0600

    Code
    * CSOM way to create MySite /personal/ sites in the  cloud

---
+++ office365-migration/office365-CreatePersonalSiteEnqueueBulk.ps1
@@ -0,0 +1,30 @@
+﻿# CSOM method to provision MySite /personal/ sites in Office 365
+
+# tenant
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
+$webUrl = "https://tenant.sharepoint.com"
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($webUrl)
+
+# admin user
+$web = $ctx.Web
+$username = "admin@tenant.onmicrosoft.com"
+$password = read-host -AsSecureString
+
+# context
+$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username,$password )
+$ctx.Load($web)
+$ctx.ExecuteQuery()
+
+# assembly
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.UserProfiles")
+$loader =[Microsoft.SharePoint.Client.UserProfiles.ProfileLoader]::GetProfileLoader($ctx)
+
+#To Get Profile
+$profile = $loader.GetUserProfile()
+$ctx.Load($profile)
+$ctx.ExecuteQuery()
+$profile 
+
+#To enqueue Profile
+$loader.CreatePersonalSiteEnqueueBulk(@("user@tenant.onmicrosoft.com")) 
+$loader.Context.ExecuteQuery()

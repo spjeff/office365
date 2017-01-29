5973233cdf3fcc4a9d02d8d26d3ad8369e52ec80
Jeff Jones <spjeff@spjeff.com>
2017-01-29 11:12:42 -0600

    Code
    VSCode - format doc

--- office365-desktop-icon/office365-desktop-icon.ps1
+++ office365-desktop-icon/office365-desktop-icon.ps1
@@ -50,7 +50,7 @@ $Shortcut.Save()
 
 #run as admin - @brianlala http://stackoverflow.com/questions/28997799/how-to-create-a-run-as-administrator-shortcut-using-powershell
 if ($runAsAdmin -like 'Y*') {
-	$bytes = [System.IO.File]::ReadAllBytes($ShortcutFile)
-	$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
-	[System.IO.File]::WriteAllBytes($ShortcutFile, $bytes)
+    $bytes = [System.IO.File]::ReadAllBytes($ShortcutFile)
+    $bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+    [System.IO.File]::WriteAllBytes($ShortcutFile, $bytes)
 }

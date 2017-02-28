174cbdd9b2e9ed9693f6cd139ce1105e9015cb6f
Jeff Jones <spjeff@spjeff.com>
2017-02-28 12:54:31 -0600

    Code
    * download all PowerBI visualizations (PBIVIZ)

---
+++ office365-PowerBI-download-PBIVIZ/office365-PowerBI-download-PBIVIZ.ps1
@@ -0,0 +1,16 @@
+﻿$path = Split-Path $MyInvocation.MyCommand.Path
+$base = "https://visuals.azureedge.net/gallery-prod/"
+$catalog = $base + "visualCatalog.json" 
+$wr = Invoke-WebRequest -Uri $catalog
+$json = $wr.Content | ConvertFrom-Json
+$client = New-Object System.Net.WebClient
+
+foreach ($j in $json) {
+   $u = $base + $j.downloadUrl
+   $u
+   $file = $path + $j.downloadUrl
+   $client.DownloadFile($u, $file)
+}
+
+$n = $json.Count
+Write-Host "Downloaded $n Files" -Fore Green

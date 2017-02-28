f5dd0983be4ce684b1bc986edd22bfe231f19fea
Jeff Jones <spjeff@spjeff.com>
2017-02-28 12:57:29 -0600

    Code
    * comments

--- office365-PowerBI-download-PBIVIZ/office365-PowerBI-download-PBIVIZ.ps1
+++ office365-PowerBI-download-PBIVIZ/office365-PowerBI-download-PBIVIZ.ps1
@@ -1,10 +1,32 @@
-﻿$path = Split-Path $MyInvocation.MyCommand.Path
+﻿<#
+.SYNOPSIS
+	Download all PBIVIZ visualization files from the MS Gallery.
+
+.DESCRIPTION
+	Download JSON feed with list of available visulizations and downloads each to the loca folder.
+
+	Comments and suggestions always welcome!  spjeff@spjeff.com or @spjeff
+.NOTES
+	File Name		: office365-PowerBI-download-PBIVIZ.ps1
+	Author			: Jeff Jones - @spjeff
+	Version			: 0.10
+	Last Modified	: 02-28-2017
+.LINK
+	Source Code
+		https://github.com/spjeff/office365/blob/master/office365-PowerBI-download-PBIVIZ/
+#>
+
+# Configure
+$path = Split-Path $MyInvocation.MyCommand.Path
 $base = "https://visuals.azureedge.net/gallery-prod/"
 $catalog = $base + "visualCatalog.json" 
+
+# Download JSON catalog
 $wr = Invoke-WebRequest -Uri $catalog
 $json = $wr.Content | ConvertFrom-Json
-$client = New-Object System.Net.WebClient
 
+# Download each PBIVIZ binary file
+$client = New-Object System.Net.WebClient
 foreach ($j in $json) {
    $u = $base + $j.downloadUrl
    $u
@@ -12,5 +34,6 @@ foreach ($j in $json) {
    $client.DownloadFile($u, $file)
 }
 
+# Summary
 $n = $json.Count
 Write-Host "Downloaded $n Files" -Fore Green

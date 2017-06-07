e2e6f1dbf24db94591ada8ddebf018e9876b839e
Jeff Jones <spjeff@spjeff.com>
2017-06-07 13:26:08 -0500

    Code
    * fn

--- office365-export-profiles/office365-export-profiles.ps1
+++ office365-export-profiles/office365-export-profiles.ps1
@@ -20,53 +20,59 @@ $dateStamp = Get-Date -Format yyyyMMdd-hhmm
 
 # Azure AD (AAD) - All Profiles
 # --------------------
-Write-Host "Azure AD (AAD)"
-$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($username, $secPassword)
-Connect-MsolService -Credential $cred
-$msolUsers = Get-MsolUser -All
-$msolUsers | Export-Csv "get-msoluser-$dateStamp.csv" -NoTypeInformation -Force
+function aadProfiles() {
+    Write-Host "Azure AD (AAD)"
+    $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ($username, $secPassword)
+    Connect-MsolService -Credential $cred
+    $msolUsers = Get-MsolUser -All
+    $msolUsers | Export-Csv "get-msoluser-$dateStamp.csv" -NoTypeInformation -Force
+}
 
 # SharePoint Online (SPO) - User Profile Service (UPS) ASMX
 # --------------------
-# Take the AdminAccount and the AdminAccount password, and create a credential
-$secpw = ConvertTo-SecureString $password -AsPlainText -Force
-$creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $secpw)
- 
-# Add the path of the User Profile Service to the SPO admin URL, then create a new webservice proxy to access it
-$proxyaddr = "$tenantAdminUrl/_vti_bin/UserProfileService.asmx?wsdl"
-$UserProfileService = New-WebServiceProxy -Uri $proxyaddr -UseDefaultCredential $false
-$UserProfileService.Credentials = $creds
- 
-# Set variables for authentication cookies
-$strAuthCookie = $creds.GetAuthenticationCookie($tenantAdminUrl)
-$uri = New-Object System.Uri($tenantAdminUrl)
-$container = New-Object System.Net.CookieContainer
-$container.SetCookies($uri, $strAuthCookie)
-$UserProfileService.CookieContainer = $container
- 
-# Sets the first User profile, at index -1
-$UserProfileResult = $UserProfileService.GetUserProfileByIndex(-1)
-$NumProfiles = $UserProfileService.GetUserProfileCount()
-$i = 1
- 
-# As long as the next User profile is NOT the one we started with (at -1)...
-$ups = @()
-While ($UserProfileResult.NextValue -ne -1) {
-    Write-Host "Examining profile $i of $NumProfiles"
-    $props = $UserProfileResult.UserProfile
-    $row = New-Object PSObject
-    foreach ($p in $props) {
-        if ($p.values.value) {
-            $row | Add-Member -MemberType NoteProperty -Name $p.Name -Value $p.values.value.ToString()
+function spoProfiles() {
+    # Take the AdminAccount and the AdminAccount password, and create a credential
+    $secpw = ConvertTo-SecureString $password -AsPlainText -Force
+    $creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $secpw)
+    
+    # Add the path of the User Profile Service to the SPO admin URL, then create a new webservice proxy to access it
+    $proxyaddr = "$tenantAdminUrl/_vti_bin/UserProfileService.asmx?wsdl"
+    $UserProfileService = New-WebServiceProxy -Uri $proxyaddr -UseDefaultCredential $false
+    $UserProfileService.Credentials = $creds
+    
+    # Set variables for authentication cookies
+    $strAuthCookie = $creds.GetAuthenticationCookie($tenantAdminUrl)
+    $uri = New-Object System.Uri($tenantAdminUrl)
+    $container = New-Object System.Net.CookieContainer
+    $container.SetCookies($uri, $strAuthCookie)
+    $UserProfileService.CookieContainer = $container
+    
+    # Sets the first User profile, at index -1
+    $UserProfileResult = $UserProfileService.GetUserProfileByIndex(-1)
+    $NumProfiles = $UserProfileService.GetUserProfileCount()
+    $i = 1
+    
+    # As long as the next User profile is NOT the one we started with (at -1)...
+    $ups = @()
+    While ($UserProfileResult.NextValue -ne -1) {
+        Write-Host "Examining profile $i of $NumProfiles"
+        $props = $UserProfileResult.UserProfile
+        $row = New-Object PSObject
+        foreach ($p in $props) {
+            if ($p.values.value) {
+                $row | Add-Member -MemberType NoteProperty -Name $p.Name -Value $p.values.value.ToString()
+            }
         }
+        $UserProfileResult = $UserProfileService.GetUserProfileByIndex($UserProfileResult.NextValue)
+        $ups += $row
+        $ups.Count
+        $i++
     }
-    $UserProfileResult = $UserProfileService.GetUserProfileByIndex($UserProfileResult.NextValue)
-    $ups += $row
-    $ups.Count
-    $i++
-}
 
-# Save CSV
-Write-Host "Writing CSV file..."
-$ups | Export-Csv "userprofileservice-asmx-$dateStamp.csv" -NoTypeInformation -Force
+    # Save CSV
+    Write-Host "Writing CSV file..."
+    $ups | Export-Csv "userprofileservice-asmx-$dateStamp.csv" -NoTypeInformation -Force
+}
+aadProfiles
+spoProfiles

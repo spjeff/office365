c70b12ef4475e50ef53a47d1efc6fe97647ff975
Jeff Jones <spjeff@spjeff.com>
2017-06-07 13:11:07 -0500

    Code
    * download UserProfileService.asmx to CSV

--- office365-export-profiles/office365-export-profiles.ps1
+++ office365-export-profiles/office365-export-profiles.ps1
@@ -1,55 +1,72 @@
 # Export Office 365 user profiles to CSV (Azure AD & SharePoint UPS)
-# Download from ... Microsoft Online Services Sign-in Assistant for IT Professionals RTW http://go.microsoft.com/fwlink/p/?LinkId=286152
-# Download from ... Windows Azure Active Directory Module for Windows PowerShell (64-bit version) http://go.microsoft.com/fwlink/p/?linkid=236297
+# --------------------
+# Download -- Microsoft Online Services Sign-in Assistant for IT Professionals RTW http://go.microsoft.com/fwlink/p/?LinkId=286152
+# Download -- Windows Azure Active Directory Module for Windows PowerShell (64-bit version) http://go.microsoft.com/fwlink/p/?linkid=236297
+# Download -- SharePoint Server 2013 Client Components CSOM SDK https://www.microsoft.com/en-us/download/details.aspx?id=35585
+
+# Plugins
 Import-Module MSOnline
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client") | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime") | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.UserProfiles") | Out-Null
 
 # Tenant
-$tenantUrl = "https://tenant-admin.sharepoint.com/"
-$userName = "admin@tenant.onmicrosoft.com" 
+$tenantAdminUrl = "https://tenant-admin.sharepoint.com/"
+$username = "admin@tenant.onmicrosoft.com"
 $password = "pass@word1"
 
+# Date stamp
+$dateStamp = Get-Date -Format yyyyMMdd-hhmm
+
 # Azure AD (AAD) - All Profiles
+# --------------------
 Write-Host "Azure AD (AAD)"
 $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($userName, $secPassword)
+$cred = New-Object System.Management.Automation.PSCredential ($username, $secPassword)
 Connect-MsolService -Credential $cred
 $msolUsers = Get-MsolUser -All
-$msolUsers | Export-Csv "azure-ad-profiles.csv"
+$msolUsers | Export-Csv "get-msoluser-$dateStamp.csv" -NoTypeInformation -Force
 
-# SharePoint Online (SPO) - All Profiles
-Write-Host "SharePoint Online (SPO)"
-$start = Get-Date
-$i = 0
-$total = $allUsers.Count
-$coll = @()
-$csv = ""
-Connect-SPOnline -Url $tenantUrl -Credentials $cred
-Connect-SPOService -Url $tenantUrl -Credential $cred
-$spoUsers = Get-SPOUser -Site $tenantUrl
-foreach ($u in $spoUsers) {
-    # Progress Tracking
-    $i++
-    $prct = [Math]::Round((($i / $total) * 100.0), 2)
-    $elapsed = (Get-Date) - $start
-    $remain = ($elapsed.TotalSeconds) / ($prct / 100.0)
-    $eta = (Get-Date).AddSeconds($remain)
-	
-    # Display
-    Write-Progress -Activity "Download SharePoint Online user profiles - ETA $eta" -Status "$prct" -PercentComplete $prct
-	
-    # Append CSV sign in name
-    $csv += $u.LoginName + ","
-    if ($i -eq 200) {
-        # Download
-        $csv = $csv.TrimEnd(",")
-        $obj = Get-SPOUserProfileProperty -Account $csv.Split(",") -ErrorAction SilentlyContinue
-        $coll += $obj
-		
-        # clear
-        $csv = ""
-        $i = 0
-        Write-Host "." -NoNewLine
+# SharePoint Online (SPO) - User Profile Service (UPS) ASMX
+# --------------------
+# Take the AdminAccount and the AdminAccount password, and create a credential
+$secpw = ConvertTo-SecureString $password -AsPlainText -Force
+$creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $secpw)
+ 
+# Add the path of the User Profile Service to the SPO admin URL, then create a new webservice proxy to access it
+$proxyaddr = "$tenantAdminUrl/_vti_bin/UserProfileService.asmx?wsdl"
+$UserProfileService = New-WebServiceProxy -Uri $proxyaddr -UseDefaultCredential $false
+$UserProfileService.Credentials = $creds
+ 
+# Set variables for authentication cookies
+$strAuthCookie = $creds.GetAuthenticationCookie($tenantAdminUrl)
+$uri = New-Object System.Uri($tenantAdminUrl)
+$container = New-Object System.Net.CookieContainer
+$container.SetCookies($uri, $strAuthCookie)
+$UserProfileService.CookieContainer = $container
+ 
+# Sets the first User profile, at index -1
+$UserProfileResult = $UserProfileService.GetUserProfileByIndex(-1)
+$NumProfiles = $UserProfileService.GetUserProfileCount()
+$i = 1
+ 
+# As long as the next User profile is NOT the one we started with (at -1)...
+$ups = @()
+While ($UserProfileResult.NextValue -ne -1) {
+    Write-Host "Examining profile $i of $NumProfiles"
+    $props = $UserProfileResult.UserProfile
+    $row = New-Object PSObject
+    foreach ($p in $props) {
+        if ($p.values.value) {
+            $row | Add-Member -MemberType NoteProperty -Name $p.Name -Value $p.values.value.ToString()
+        }
     }
+    $UserProfileResult = $UserProfileService.GetUserProfileByIndex($UserProfileResult.NextValue)
+    $ups += $row
+    $ups.Count
     $i++
 }
-$coll | Export-Csv "sharepoint-profiles.csv"
+
+# Save CSV
+Write-Host "Writing CSV file..."
+$ups | Export-Csv "userprofileservice-asmx-$dateStamp.csv" -NoTypeInformation -Force

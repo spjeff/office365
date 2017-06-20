65dab17814834a730c84dd7b855ed4ddbc6b7b22
Jeff Jones <spjeff@spjeff.com>
2017-06-20 10:35:35 -0500

    Code

--- office365-gpo/onedrive-gpo.ps1
+++ office365-gpo/onedrive-gpo.ps1
@@ -6,8 +6,10 @@
 	Disables Members Can Share setting
 #>
 
-$datestamp = (Get-Date).tostring("yyyy-MM-dd-hh-mm-ss")
-Start-Transcript "onedrive-gpo-$datestamp.csv"
+$startTime = (Get-Date)
+$datestamp = $startTime.ToString("yyyy-MM-dd-hh-mm-ss")
+
+Start-Transcript "OneDriveGPO-$datestamp.csv"
 
 # Plugins
 Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue | Out-Null
@@ -30,32 +32,43 @@ Function GrantSCA ($url) {
     foreach ($admin in $admins) {
         $user = Get-SPOUser -Site $site -LoginName $admin
         if (!$user.IsSiteAdmin) {
+            $displayName = $user.DisplayName
             Set-SPOUser -Site $site -LoginName $admin -IsSiteCollectionAdmin $true
+            Write-Host "$displayName granted SCA" -Fore Green
+        } else {
+            Write-Host "$displayName already SCA" -Fore Yellow
         }
     }
 }
 
 # Main
 Function Main() {
-
     # Credentials
-   	$secpw = ConvertTo-SecureString -String $settings.settings.adminPass -AsPlainText -Force
-    $cred = New-Object System.Management.Automation.PSCredential ($settings.settings.adminUserName, $secpw)
+   	$cloudpw = ConvertTo-SecureString -String $settings.settings.adminPass -AsPlainText -Force
+    $cloudCred = New-Object System.Management.Automation.PSCredential ($settings.settings.adminUserName, $cloudpw)
     Connect-PnPOnline $adminURL
-    Connect-SPOService -URL $adminURL -Credential $cred
+    Connect-SPOService -URL $adminURL -Credential $cloudCred
 
     # Azure AD (AAD) - All Profiles
     Write-Host "Azure AD (AAD)"
     $tracker = @()
-    Connect-MsolService -Credential $cred
-    $msolUsers = Get-MsolUser -All
+    Connect-MsolService -Credential $cloudCred
+    #$msolUsers = Get-MsolUser -All -EnabledFilter EnabledOnly
+	$msolUsers = Get-MsolUser -EnabledFilter EnabledOnly -MaxResults 10
+    $personalURLs = @()
     foreach ($msolUser in $msolUsers) {
         $profile = Get-PNPUserProfileProperty -Account $msolUser.UserPrincipalName
-        $personalURLs += New-Object -TypeName PSObject -Props {@('UPN'=$msolUser.UserPrincipalName; 'Url'=$profile.PersonalURL; 'State'='New')}
+                
+        if ($profile.PersonalURL -like "https://tenant-my.sharepoint.com/personal/*") {
+            $profileObj = New-Object -TypeName PSObject -Prop (@{"UPN"=$msolUser.UserPrincipalName; "Url"=$profile.PersonalURL.TrimEnd("/"); "State"="New"})
+            $tracker += $profileObj
+        }
     }
 
     # Grant Site Collection Admin
-    foreach ($url in $csv.SiteUrl) {
+    foreach ($personalURL in $tracker) {
+        $personalURL
+        $url = $personalURL.Url
         GrantSCA $url
     }
 
@@ -82,25 +95,27 @@ Function Main() {
         $AllWebs += $RootWeb
  
         # Member Invites Disabled
-        foreach ($SubWeb in $SubWebs) {
-            $Sub = Get-PnPWeb -Identity $SubWeb -Includes MembersCanShare, AssociatedMemberGroup
-            $AllWebs += $Sub
-            $MemberGroupTitle = $Sub.AssociatedMemberGroup.Title
-            if ($MemberGroupTitle) {
-                $MemberGroup = Get-PnPGroup -Identity $MemberGroupTitle -Includes AllowMembersEditMembership
-                if ($MemberGroup.AllowMembersEditMembership) {
-                    $GroupTitle = $MemberGroup.Title
-                    Set-PnPGroup -Identity $GroupTitle -AllowMembersEditMembership $False
-                    Write-Host "$GroupTitle - Member Invites Disabled" -Fore Yellow
-                }
-                else {
-                    Write-Host "$GroupTitle - Member Invites Already Disabled" -Fore DarkCyan
-                }
-            }
-            else {
-                Write-Host "No Member Group Found" -Fore Red
-            }
-        }
+		if ($SubWebs) {
+			foreach ($SubWeb in $SubWebs) {
+				$Sub = Get-PnPWeb -Identity $SubWeb -Includes MembersCanShare, AssociatedMemberGroup
+				$AllWebs += $Sub
+				$MemberGroupTitle = $Sub.AssociatedMemberGroup.Title
+				if ($MemberGroupTitle) {
+					$MemberGroup = Get-PnPGroup -Identity $MemberGroupTitle -Includes AllowMembersEditMembership
+					if ($MemberGroup.AllowMembersEditMembership) {
+						$GroupTitle = $MemberGroup.Title
+						Set-PnPGroup -Identity $GroupTitle -AllowMembersEditMembership $False
+						Write-Host "$GroupTitle - Member Invites Disabled" -Fore Yellow
+					}
+					else {
+						Write-Host "$GroupTitle - Member Invites Already Disabled" -Fore DarkCyan
+					}
+				}
+				else {
+					Write-Host "No Member Group Found" -Fore Red
+				}
+			}
+		}
 
         # Disable MembersCanShare in All Webs
         foreach ($Web in $AllWebs) {
@@ -118,6 +133,7 @@ Function Main() {
 
         # Enable Audit
         $audit = Get-PnPAuditing
+        
         if ($audit.AuditFlags -eq "All") {
             Write-Host "Audit OK" -Fore Green
         }
@@ -129,15 +145,13 @@ Function Main() {
         # Add Custom JS
         $scriptName = $settings.settings.scriptName
         $scriptUrl = $settings.settings.scriptUrl
-        Write-Host "Found $scriptName" -Fore Green
-        Write-Host "Found $scriptUrl" -Fore Green
 
         # Detect Custom JS
-        $found = Get-PnPJavaScriptLink -Scope All |? {$_.Name -eq $scriptName}
-        if ($found) {
+        $jsFound = Get-PnPJavaScriptLink -Scope All |? {$_.Name -eq $scriptName}
+        if ($jsFound) {
             # Found
             Write-Host "Found $scriptName" -Fore Green
-            $found | ft -a
+            $jsFound | Format-Table -a
         }
         else {
             # Add JS Script Link missing
@@ -146,21 +160,28 @@ Function Main() {
         }
     }
 
+    #Open session to local host
+    $farmpw = ConvertTo-SecureString -String $settings.settings.farmPass -AsPlainText -Force
+    $farmCred = New-Object System.Management.Automation.PSCredential ($settings.settings.farmUserName, $farmpw)
+    $s = New-PSSession -ComputerName $env:COMPUTERNAME -Credential $farmCred -Authentication Credssp
+
     # Parallel PNP to apply standard settings to destiation sites
     $numWorkers = $settings.settings.workers
+    $pending = $tracker |? {$_.State -ne "Complete"}
     do {
         #Pull active workers
         $pending = $tracker |? {$_.State -ne "Complete"}
         $activeWorkers = Get-Job |? {$_.State -eq "Running" -or $_.State -eq "NotStarted"}
-
-        if ($activeWorkers -lt $numWorkers) {
+        
+        if ($activeWorkers.count -lt $numWorkers -and $pending) {
             #Create new worker
             $nextSite = $pending[0].Url
             $found =  $tracker |? {$_.UPN -eq $pending[0].UPN}
             $found.State = "Complete"
             Write-Host "Next site..." -ForegroundColor Yellow
-            Invoke-Command -ScriptBlock $sb -ArgumentList $nextSite -AsJob
+            Invoke-Command -ScriptBlock $sb -ArgumentList $nextSite -AsJob -Session $s
         }
+        $activeWorkers
         
         #Clean up
         $idleWorkers = Get-Job |? {$_.State -ne "Running" -and $_.State -ne "NotStarted"}
@@ -169,4 +190,13 @@ Function Main() {
     while ($pending)
 }
 Main
+
+# Time stamps
+$endTime = (Get-Date)
+$totalSec = (($endTime -$startTime).totalseconds)
+$timeSpan = [timespan]::fromseconds($totalSec)
+$timeHours = "{0:HH:mm:ss}" -f ([datetime]$timeSpan.Ticks)
+
+# Log
+Write-Host "Total Elapsed Time: $timeHours" -ForegroundColor Green
 Stop-Transcript

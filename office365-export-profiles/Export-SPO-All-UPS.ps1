c73f1a76616c4bfb0c9682ce798aea5a42b1e242
Jeff Jones <spjeff@spjeff.com>
2021-01-17 09:22:16 -0600

    Latest

--- office365-export-profiles/Export-SPO-All-UPS.ps1
+++ office365-export-profiles/Export-SPO-All-UPS.ps1
@@ -1,3 +1,19 @@
+# from https://gist.github.com/asadrefai/ecfb32db81acaa80282d
+# from https://www.microsoft.com/en-us/download/confirmation.aspx?id=42038
+# installed file [sharepointclientcomponents_16-6906-1200_x64-en-us] 
+Try{
+    Add-Type -Path 'C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll'
+    Add-Type -Path 'C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll'
+    Add-Type -Path 'C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.UserProfiles.dll'
+} 
+catch {
+    Write-Host $_.Exception.Message
+    Write-Host "No further parts of the migration will be completed" 
+}
+# [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
+# [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime")
+# [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.UserProfiles")
+
 # from https://community.idera.com/database-tools/powershell/ask_the_experts/f/learn_powershell_from_don_jones-24/2824/exporting-key-value-pair-using-export-csv-cmdlet
 function ConvertTo-Object($hashtable) 
 {
@@ -12,7 +28,7 @@ $hashtable = $ups
 
 # from https://sharepoint.stackexchange.com/questions/108664/powershell-script-for-user-profile-properties-in-sharepoint-online-2013
 $SiteURL = "https://spjeff.sharepoint.com"
-Connect-pnponline -Url $SiteURL -UseWebLogin
+Connect-pnponline -Url $SiteURL
 $Context = Get-PNPContext
 
 #Identify users in the Site Collection
@@ -21,7 +37,7 @@ $Context.Load($Users)
 $Context.ExecuteQuery()
 
 #Create People Manager object to retrieve profile data
-$PeopleManager = New-Object   Microsoft.SharePoint.Client.UserProfiles.PeopleManager($Context)
+$PeopleManager = New-Object Microsoft.SharePoint.Client.UserProfiles.PeopleManager($Context)
 $coll = @()
 $i=0
 Foreach ($User in $Users)
@@ -40,5 +56,5 @@ $Context.ExecuteQuery()
     }  
 }
 
-$coll | Export-Csv "D:\Export-SPO-All-UPS.csv" -NoTypeInformation
+$coll | Export-Csv "Export-SPO-All-UPS.csv" -NoTypeInformation
 Write-Host "DONE"
